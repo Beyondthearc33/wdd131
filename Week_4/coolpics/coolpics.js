083d36a601688224c685e4c3970a62ffc38683a7
Beyondthearc33 <luke.lucas001@gmail.com>
2025-05-16 15:06:27 -0600

    Draft

--- Week_4/coolpics/coolpics.js
+++ Week_4/coolpics/coolpics.js
@@ -1,7 +1,7 @@
 const menuButton = document.querySelector("#menu-button");
 function toggleMenu() {
-  const menu = document.querySelector("#menu");
-  menu.classList.toggle("hide");
+    const menu = document.querySelector("#menu");
+    menu.classList.toggle("hide");
 }
 
 menuButton.addEventListener("click", toggleMenu);
@@ -11,8 +11,31 @@ function handleResize() {
     if (window.innerWidth >= 700) {
         menu.classList.remove("hide");
     } else {
-        menu.classList.add("hide");    
+        menu.classList.add("hide");
     }
 }
 
 window.addEventListener("resize", handleResize);
+
+
+const images = document.querySelectorAll("figure img");
+const dialog = document.querySelector("#dialog");
+
+images.forEach(image => {
+    image.addEventListener("click", showDialog);
+})
+
+function showDialog(event) {
+    const image = event.currentTarget;
+    const imgSrc = image.getAttribute("src");
+    const dialogImg = document.querySelector("#dialog-img");
+    dialogImg.setAttribute("src", imgSrc);
+    dialog.showModal();   
+}
+
+document.querySelector("#dialog-close-btn").addEventListener("click", closeDialog);
+
+function closeDialog() {
+    dialog.close();
+}
+
